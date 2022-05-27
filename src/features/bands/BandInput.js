0b0ef544d032cab049a76235d68a946253343fa6
Marlena Tsang <Marlena345@gmail.com>
2022-05-26 23:48:34 -0400

    commit tree fdc2a10727ec3b638f14b3af3d33e0d566c2b2f8

--- src/features/bands/BandInput.js
+++ src/features/bands/BandInput.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
-function BandInput() {
-  return <div>Band Input</div>;
+function BandInput({onBandSubmit}) {
+  const [band, setBand] = useState("");
+
+  function handleChange(e){
+    setBand(e.target.value)
+  };
+
+  function handleSubmit(e){
+    e.preventDefault();
+    onBandSubmit(band)
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label type="name">name
+        <input 
+          type="text" 
+          name={band} 
+          value={band} 
+          onChange={handleChange}
+        />
+        </label>
+        <button type="submit">Add Band</button>
+      </form>
+    </div>
+    )
 }
 
 export default BandInput;
--- src/features/bands/BandsContainer.js
+++ src/features/bands/BandsContainer.js
@@ -1,7 +1,27 @@
 import React from "react";
+import BandInput from "./BandInput";
+import {useDispatch, useSelector} from "react-redux";
+import {bandAdded} from "./bandsSlice";
 
 function BandsContainer() {
-  return <div>BandsContainer</div>;
+  const dispatch = useDispatch();
+  const bands = useSelector((state)=>state.bands.entities)
+
+  function onBandSubmit(name){
+    dispatch(bandAdded(name))
+  }
+
+  return (
+    <div>
+      <BandInput onBandSubmit={onBandSubmit} />
+      <ul>
+        {bands.map((band)=>(
+          <li key={band}>{band}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default BandsContainer;
+// this is the child of App. should act as your connection to the Redux store(actions to dispatch)
